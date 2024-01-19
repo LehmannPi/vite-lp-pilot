b05138c73426e740c40f39fd9adfac211116cf77
LehmannPi <filipelehmannp@gmail.com>
2024-01-19 20:30:50 -0300

    Project folder structure

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import AboutSection from '../../components/AboutSection';
+import ContactSection from '../../components/ContactSection';
+import MainSection from '../../components/MainSection';
+import PriceSection from '../../components/PriceSection';
+import ProgramSection from '../../components/ProgramSection';
+
+export default function App() {
+  return (
+    <>
+      <MainSection />
+      <AboutSection />
+      <ProgramSection />
+      <PriceSection />
+      <ContactSection />
+    </>
+  );
+}
