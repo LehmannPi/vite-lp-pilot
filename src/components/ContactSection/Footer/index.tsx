78420405757fa005cd4874bfb2ab81e83a763176
LehmannPi <filipelehmannp@gmail.com>
2024-02-14 13:52:27 -0300

    feat: add final settings in contact section

--- src/components/ContactSection/Footer/index.tsx
+++ src/components/ContactSection/Footer/index.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 const Footer = ({ linkToArts }: Props) => {
   return (
-    <footer>
+    <footer style={{ textAlign: 'center' }}>
       Designed by Filipe Lehmann. Arts by <a href={linkToArts}>Icon8</a>
     </footer>
   );
--- src/components/ContactSection/index.tsx
+++ src/components/ContactSection/index.tsx
@@ -1,16 +1,32 @@
+import { ColoredIG } from '../common/icons/contact/ColooredIG';
+import { ColoredFB } from '../common/icons/contact/ColoredFB';
+import { ColoredLinkedIn } from '../common/icons/contact/ColoredLinkedIn';
+import { ColoredX } from '../common/icons/contact/ColoredX';
+import { ContactForm } from './ContactForm';
 import Footer from './Footer';
+import './index.scss';
 
 export default function ContactSection() {
   return (
-    <>
+    <div className="contact-section">
       <h1>Contact me</h1>
-      <div style={{ display: 'flex' }}>
-        <span>FB</span>
-        <span>LI</span>
-        <span>IG</span>
-        <span>X</span>
+      <ContactForm submit={() => alert('Simulação de mensagem enviada.')} />
+      {/* <ContactForm submit={(values) => alert(JSON.stringify(values))} /> */}
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          gap: '40px',
+          cursor: 'pointer',
+          margin: '40px auto',
+        }}
+      >
+        <ColoredFB />
+        <ColoredLinkedIn />
+        <ColoredIG />
+        <ColoredX />
       </div>
       <Footer linkToArts="" />
-    </>
+    </div>
   );
 }
