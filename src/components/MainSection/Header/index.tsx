23decbdb63935100a883c576b1c2f6803828e3cf
LehmannPi <filipelehmannp@gmail.com>
2024-02-17 16:22:32 -0300

    feat: adds navbar on mobile and fix minor responsivity issue

--- src/components/MainSection/Header/index.tsx
+++ src/components/MainSection/Header/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { NavbarIcon } from '../../common/icons/NavbarIcon';
 import { InstagramIcon } from '../../common/icons/header/FacebookIcon';
 import { FacebookIcon } from '../../common/icons/header/InstagramIcon';
 import { LinkedInIcon } from '../../common/icons/header/LinkedInIcon';
@@ -10,28 +12,59 @@ export default function Header() {
     const positionTop = matches[0].getBoundingClientRect().top;
     window.scrollTo({ top: positionTop, behavior: 'smooth' });
   }
+
+  const [mobile, setMobile] = useState(false);
+
+  function handleMobileNav() {
+    setMobile(!mobile);
+  }
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        width: 'full',
-        justifyContent: 'space-around',
-        paddingTop: '37px',
-      }}
-    >
-      <p className="header-title">Filipe's Editor</p>
-      <div className="header-central">
-        <span onClick={() => scrollToSection('main')}>Main</span>
-        <span onClick={() => scrollToSection('about')}>About</span>
-        <span onClick={() => scrollToSection('program')}>Program</span>
-        <span onClick={() => scrollToSection('price')}>Price</span>
-        <span onClick={() => scrollToSection('contact')}>Contact</span>
+    <>
+      <div className="header-desktop">
+        <p className="header-desktop-title">Filipe's Editor</p>
+        <div className="header-desktop-central">
+          <button onClick={() => scrollToSection('main')}>Main</button>
+          <button onClick={() => scrollToSection('about')}>About</button>
+          <button onClick={() => scrollToSection('program')}>Program</button>
+          <button onClick={() => scrollToSection('price')}>Price</button>
+          <button onClick={() => scrollToSection('contact')}>Contact</button>
+        </div>
+        <div className="header-desktop-social">
+          <InstagramIcon />
+          <FacebookIcon />
+          <LinkedInIcon />
+        </div>
       </div>
-      <div className="header-social">
-        <InstagramIcon />
-        <FacebookIcon />
-        <LinkedInIcon />
-      </div>
-    </div>
+      {mobile ? null : (
+        <div className="header-mobile">
+          <p className="header-mobile-title">Filipe's Editor</p>
+          <button
+            onClick={handleMobileNav}
+            className="header-mobile-nav-button"
+          >
+            <NavbarIcon />
+          </button>
+
+          {/* <div className="header-mobile-social">
+          <InstagramIcon />
+          <FacebookIcon />
+          <LinkedInIcon />
+        </div> */}
+        </div>
+      )}
+      {mobile ? (
+        <>
+          <div onClick={handleMobileNav} className="mobile-overlay" />
+          <div className="header-mobile-central">
+            <button onClick={() => scrollToSection('main')}>Main</button>
+            <button onClick={() => scrollToSection('about')}>About</button>
+            <button onClick={() => scrollToSection('program')}>Program</button>
+            <button onClick={() => scrollToSection('price')}>Price</button>
+            <button onClick={() => scrollToSection('contact')}>Contact</button>
+          </div>
+        </>
+      ) : null}
+    </>
   );
 }
